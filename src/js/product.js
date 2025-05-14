0f1309e8bde11570b6ae0b02c9b670c3f43a4df9
Uchenna77-dev <uchennaonuorah40@gmail.com>
2025-05-14 13:38:12 +0100

    commit

--- src/js/product.js
+++ src/js/product.js
@@ -18,10 +18,6 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
-
-  import { getParam } from "./utils.mjs";
-  import ProductData from "./ProductData.mjs";
-  import ProductDetails from "./ProductDetails.mjs";
   
   const dataSource = new ProductData("tents");
   const productID = getParam("product");

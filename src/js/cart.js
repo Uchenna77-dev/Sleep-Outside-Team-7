1fb0e2e1b1b0d9bce9ba7bb21ff58a6160e975e5
Uchenna77-dev <uchennaonuorah40@gmail.com>
2025-05-23 05:49:07 +0100

    commit

--- src/js/cart.js
+++ src/js/cart.js
@@ -8,6 +8,17 @@ function renderCartContents() {
   const cartItems = getLocalStorage("so-cart") || [];
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+  // If cart is not empty, show footer and calculate total
+  if (cartItems.length > 0) {
+    document.querySelector(".cart-footer").classList.remove("hide");
+
+    // Calculate total
+    const total = cartItems.reduce((sum, item) => sum + Number(item.FinalPrice), 0);
+
+    // Insert total into HTML
+    document.querySelector(".cart-total").textContent = `Total: $${total.toFixed(2)}`;
+  }
 }
 
 function cartItemTemplate(item) {
